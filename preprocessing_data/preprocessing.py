78e37fcf1073b699243f5751035004e2eb97f3c9
Adel Samigullin <shi.samadel@gmail.com>
2020-04-02 20:20:53 +0300

    Debuged

--- preprocessing_data/preprocessing.py
+++ preprocessing_data/preprocessing.py
@@ -8,11 +8,12 @@ from preprocessing_data import scaling
 
 class PreProcessing:
     def __init__(self, dataset, index_target=None):
-        self.dataset = dataset
+        self.dataset = pd.DataFrame(dataset)
         if index_target is None:
             index_target = -1
         self.target = np.array(self.dataset[self.dataset.columns[index_target]], dtype=np.object)
-        self.np_dataset = np.array(self.dataset.drop(self.dataset.columns[-1], 1), dtype=np.object)
+        self.np_dataset = np.array(self.dataset.drop(self.dataset.columns[index_target], 1), dtype=np.object)
+        self.one_hot_features = []
 
     def processing_missing_values(self, to='auto', features=None):
         base_null = ['null', 'NULL', 'NaN', 'nan', '-', '?']
@@ -24,7 +25,8 @@ class PreProcessing:
                 index_missing_values = []
                 index_filled_values = []
                 for sample in range(len(self.np_dataset)):
-                    if self.np_dataset[sample, feature] in base_null or np.isnan(self.np_dataset[sample, feature]):
+                    if self.np_dataset[sample, feature] in base_null or type(self.np_dataset[sample, feature]) == str \
+                            or np.isnan(self.np_dataset[sample, feature]):
                         index_missing_values.append(sample)
                     else:
                         index_filled_values.append(sample)
@@ -55,10 +57,14 @@ class PreProcessing:
                     if len(np.array(index_missing_values)) != 0:
                         self.np_dataset[np.array(index_missing_values), feature] = value
         else:
-            self.np_dataset = self.np_dataset[np.logical_not(np.isnan(self.np_dataset))]
+            full_dataset = np.concatenate((self.np_dataset, self.target[:,None]), axis=1)
+            full_dataset_without_nan = np.array(pd.DataFrame(full_dataset).dropna())
+            self.np_dataset = full_dataset_without_nan[:,:-1]
+            self.target = full_dataset_without_nan[:,-1:]
         return self.np_dataset
 
     def handling_outliners(self, method=None, factor=3, features=None):
+        # TODO mean and median
         if features is None:
             features = range(len(self.np_dataset[0]))
         for feature in features:
@@ -97,65 +103,72 @@ class PreProcessing:
         if features is None:
             features = range(len(self.np_dataset[0]))
         for feature in features:
-            if type_transform == 'log':
-                self.np_dataset[:, feature] = to_log(self.np_dataset[:, feature], arg)
-            elif type_transform == 'box-cox':
-                self.np_dataset[:, feature] = to_box_cox(self.np_dataset[:, feature])
-        return self.np_dataset
+            if type(self.np_dataset[0,feature]) != str:
+                print(self.np_dataset[:, feature])
+                if type_transform == 'log':
+                    self.np_dataset[:, feature] = to_log(self.np_dataset[:, feature], arg)
+                elif type_transform == 'box-cox':
+                    self.np_dataset[:, feature] = to_box_cox(self.np_dataset[:, feature])
+            return self.np_dataset
 
     def scaling(self, type_scale='norm', features=None):
         if features is None:
             features = range(len(self.np_dataset[0]))
         for feature in features:
-            if type_scale == 'norm':
-                self.np_dataset[:, feature] = scaling.normalization(self.np_dataset[:, feature])
-            elif type_scale == 'stand':
-                self.np_dataset[:, feature] = scaling.standardization(self.np_dataset[:, feature])
-            elif type_scale == 'l2-norm':
-                self.np_dataset[:, feature] = scaling.l2_normalized(self.np_dataset[:, feature])
-        return self.np_dataset
+            if type(self.np_dataset[0, feature]) != str :
+                if type_scale == 'norm':
+                    self.np_dataset[:, feature] = scaling.normalization(self.np_dataset[:, feature])
+                elif type_scale == 'stand':
+                    self.np_dataset[:, feature] = scaling.standardization(self.np_dataset[:, feature])
+                elif type_scale == 'l2-norm':
+                    self.np_dataset[:, feature] = scaling.l2_normalized(self.np_dataset[:, feature])
+            return self.np_dataset
 
-    def preprocessing_manager(self, **kwargs):
-        if kwargs['processing_missing_values']:
-            if not kwargs['processing_missing_values']['to']:
+    def preprocessing_manager(self, kwargs):
+        if 'processing_missing_values' in kwargs:
+            if 'to' not in kwargs['processing_missing_values']:
                 kwargs['processing_missing_values']['to'] = 'auto'
-            if not kwargs['processing_missing_values']['features']:
+            if 'features' not in kwargs['processing_missing_values']:
                 kwargs['processing_missing_values']['features'] = None
             self.processing_missing_values(kwargs['processing_missing_values']['to'],
                                            kwargs['processing_missing_values']['features'])
-        if kwargs['handling_outliners']:
-            if not kwargs['handling_outliners']['method']:
+        if 'handling_outliners' in kwargs:
+            if 'method' not in kwargs['handling_outliners']:
                 kwargs['handling_outliners']['method'] = None
-            if not kwargs['handling_outliners']['factor']:
+            if 'factor' not in kwargs['handling_outliners']:
                 kwargs['handling_outliners']['factor'] = 3
-            if not kwargs['handling_outliners']['features']:
+            if 'features' not in kwargs['handling_outliners']:
                 kwargs['handling_outliners']['features'] = None
             self.handling_outliners(kwargs['handling_outliners']['method'],kwargs['handling_outliners']['factor'],
                                     kwargs['handling_outliners']['features'])
-        if kwargs['binning']:
-            if not kwargs['binning']['n_bins']:
+        if 'binning' in kwargs:
+            if 'n_bins' not in kwargs['binning']:
                 kwargs['binning']['n_bins'] = 3
-            if not kwargs['binning']['type_binning']:
+            if 'type_binning' not in kwargs['binning']:
                 kwargs['binning']['type_binning'] = 'equal'
-            if not kwargs['binning']['features']:
+            if 'features' not in kwargs['binning']:
                 kwargs['binning']['features'] = None
             self.binning(kwargs['binning']['n_bins'],kwargs['binning']['type_binning'],kwargs['binning']['features'])
-        if kwargs['transform']:
-            if not kwargs['transform']['type_transform']:
+        if 'transform' in kwargs:
+            if 'type_transform' not in kwargs['transform']:
                 kwargs['transform']['type_transform'] = 'log'
-            if not kwargs['transform']['arg']:
+            if 'arg' not in kwargs['transform']:
                 kwargs['transform']['arg'] = 10
-            if not kwargs['transform']['features']:
+            if 'features' not in kwargs['transform']:
                 kwargs['transform']['features'] = None
             self.transform(kwargs['transform']['type_transform'], kwargs['transform']['arg'],
                            kwargs['transform']['features'])
-        if kwargs['scaling']:
-            if not kwargs['scaling']['type_scale']:
+        if 'scaling' in kwargs:
+            if 'type_scale' not in kwargs['scaling']:
                 kwargs['scaling']['type_scale'] = 'norm'
-            if not kwargs['scaling']['features']:
+            if 'features' not in kwargs['scaling']:
                 kwargs['scaling']['features'] = None
             self.scaling(kwargs['scaling']['type_scale'], kwargs['scaling']['features'])
-
+        self.one_hot_features = []
+        for index in range(len(self.np_dataset[0,:])):
+            if type(self.np_dataset[:,index]) == str:
+                self.one_hot_features.append(index)
+        return self.np_dataset
 
 
 
@@ -168,15 +181,18 @@ if __name__ == '__main__':
     # b = np.random.randint(0, 2, (10,1))
     # dataset = np.concatenate((a,b),axis=1)
     # print(dataset)
-    # pp = PreProcessing(pd.DataFrame(dataset),-1)
+
     # q = pp.binning(3, features=[0])
     # print(q)
-    df = pd.read_csv('../datasets/housing.csv')
-    print(df)
-    print(type(np.array(df)[0,-2]))
-    # a = np.array([['1',2],['t',3]], dtype=np.object)
-    # print(a)
-
+    # df = pd.read_csv('../datasets/housing.csv')
+    a = np.array([[1,2,'o',1],[3,4,'p',0],[np.nan,0,'q',0],[4,100,'f',1]],dtype=np.object)
+    pp = PreProcessing(a, -1)
+    print(pp.np_dataset)
+    rules = {'processing_missing_values':{'to':'del'}, 'handling_outliners':{'method':None, 'features':[1]},
+             'binning':{'n_bins':3, 'features':[1]},'transform':{}}
+    pp.preprocessing_manager(rules)
+    print(pp.np_dataset)
+    print(pp.target)
 
                     
 
